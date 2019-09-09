13d30aca839acd797f41f6f0821bff0f0e504d44
JozabadV <55096903+JozabadV@users.noreply.github.com>
2019-09-09 12:13:43 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculadora</title>
+</head>
+<body>
+	<h1 id="titulo"> Calculadora</h1>
+
+	<p id="subtitulo">Ingreso de datos</p> 
+
+	<form method="POST">
+		<p id="numero">Numero 1: <input type="text" name=""></p>
+		<p id="numero">Numero 2: <input type="text" name=""></p>
+		<input type="button" name="calcular" value="Calcular">
+		<p id="numero">Resultado: <input type="text" name=""></p>
+	</form>
+
+	<?php 
+	?>
+	
+</body> 
+</html>
+
